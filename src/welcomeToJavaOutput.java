54e45bbef6d841ef60a1690a9641e0af0da13269
Cadams98 <christianadams98@icloud.com>
2023-09-21 00:18:04 -0400

    outputThreeMessages

---
+++ src/welcomeToJavaOutput.java
@@ -0,0 +1,7 @@
+public class welcomeToJavaOutput {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to computer Science");
+        System.out.println("Programming is fun");
+    }
+}
